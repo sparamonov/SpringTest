5ea286b54f017599592db04894455978beb80f8c
wibl <035535@gmail.com>
2018-10-29 18:12:37 +0500

    added h2database

---
+++ src/main/java/ru/neyvabank/fileprocessing/rest/User.java
@@ -0,0 +1,14 @@
+package ru.neyvabank.fileprocessing.rest;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+}
